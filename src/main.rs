2663d9fba5ff4111826540bb681da8ce0c0a5128
MarcoStrijker <101836304+MarcoStrijker@users.noreply.github.com>
2024-04-17 13:20:54 +0200

    Added test in main

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,59 @@ mod utils;
 
 use crate::lib::Board;
 
-use crate::solvers::{DirectSolvers, LastRemainingCellBlock};
-
+use crate::solvers::*;
+use crate::utils::brute_force;
 
 
 fn main() {
+    let start = String::from("400000938032094100095300240370609004529001673604703090957008300003900400240030709");
+    // let start = String::from("065370002000001370000640800097004028080090001100020940040006700070018050230900060");
+    let mut board = Board::from_string(&start);
+    board.print_board();
+
+    let mut new_board = LastRemainingCell::calculate(&mut board);
+    println!("LastRemainingCell");
+    new_board.print_board();
+
+    let mut newest_board = Naked::calculate(&mut board);
+    println!("Naked");
+    newest_board.print_board();
+
+    let mut new_newest = Hidden::calculate(&mut board);
+    println!("Hidden");
+    new_newest.print_board();
+
+    let mut new_new_newer = Pointing::calculate(&mut board);
+    println!("Pointing");
+    new_new_newer.print_board();
+
+    brute_force(Board::from_string(&start)).print_board();
 
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::utils::brute_force;
     use super::*;
 
     #[test]
-    fn full() {
+    fn solve_via_brute_force() {
+        let mut board: Board;
+        let puzzles = utils::import_puzzles_from_file();
+
+        for puzzle in puzzles {
+            board = Board::from_string(&puzzle[0]);
+
+            for _ in 0..100 {
+                board = brute_force(board);
+            }
+
+            assert_eq!(board.to_string(), puzzle[1]);
+        }
+    }
+
+    #[test]
+    fn solve_via_direct_solver() {
         let mut board: Board;
         let puzzles = utils::import_puzzles_from_file();
 
